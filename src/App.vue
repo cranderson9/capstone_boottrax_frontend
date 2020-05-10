ec6b7208b62e7b5d6b693bf7107254bd6c357416
cale <caleanderson9@gmail.com>
2020-05-10 10:30:59 -0500

    add images gallery to hikes index

--- src/App.vue
+++ src/App.vue
@@ -32,6 +32,7 @@
   </div>
 </template>
 
+
 <script>
 export default {
   methods: {
--- src/views/hikesIndex.vue
+++ src/views/hikesIndex.vue
@@ -9,11 +9,46 @@
       <p>End Location: {{hike.end_location}}</p>
       <p>Miles: {{hike.miles}}</p>
       <p>Notes: {{hike.notes}}</p>
-      <img v-bind:src="hike.pictures[0].picture_file">
+      <!-- <img v-bind:src="hike.pictures[0].picture_file"> -->
+
+      <!-- Picture gallery here  -->
+      <div class="row">
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="/w3images/lights.jpg">
+        <img src="https://picsum.photos/id/10/200" alt="Lights" style="width:100%">
+        <div class="caption">
+          <p>Lorem ipsum...</p>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="/w3images/nature.jpg">
+        <img src="https://picsum.photos/id/1003/200" alt="Nature" style="width:100%">
+        <div class="caption">
+          <p>Lorem ipsum...</p>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="/w3images/fjords.jpg">
+        <img src="https://picsum.photos/id/1006/200" alt="Fjords" style="width:100%">
+        <div class="caption">
+          <p>Lorem ipsum...</p>
+        </div>
+      </a>
+    </div>
+  </div>
+</div>
       <hr>
     </div>
   </div>
 </template>
+
 
 <style>
 </style>
